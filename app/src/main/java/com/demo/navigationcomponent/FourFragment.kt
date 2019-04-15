55792788275e074d3bc711f48ae436cac66ffb66
yangzc <yangzcorange@163.com>
2019-04-15 17:28:23 +0800

    增加两个Fragment

--- app/src/main/java/com/demo/navigationcomponent/FourFragment.kt
+++ app/src/main/java/com/demo/navigationcomponent/FourFragment.kt
@@ -10,27 +10,26 @@ import android.widget.Button
 import androidx.navigation.Navigation
 
 
-
 /**
  * @author yangzc
  *
  */
-class OneFragment : Fragment() {
-    private val logTag = "OneFragment"
+class FourFragment : Fragment() {
+    private val logTag = "FourFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        return inflater.inflate(R.layout.fragment_four, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        view?.findViewById<Button>(R.id.button_one)?.setOnClickListener {
-            view?.let { Navigation.findNavController(it).navigate(R.id.twoFragment)
+        view?.findViewById<Button>(R.id.button_four)?.setOnClickListener {
+            view?.let {
+                Navigation.findNavController(it).navigate(R.id.oneFragment)
             }
         }
     }
-
 
 }
